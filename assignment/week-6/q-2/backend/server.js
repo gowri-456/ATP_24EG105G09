36bfec1aa7d540681ba541ff6208d335b9d529c6
gowri-456 <gowripriyachekka12345@gmail.com>
2026-05-16 15:11:16 +0530

    Add files via upload

---
+++ assignment/week-6/q-2/backend/server.js
@@ -0,0 +1,50 @@
+import exp from "express";
+import { connect } from "mongoose";
+import { empRoute } from "./EmpApp.js";
+import cors from "cors";
+
+const app = exp();
+const PORT = process.env.PORT || 4000;
+const MONGO_URL = process.env.MONGO_URL || "mongodb://localhost:27017/empdb";
+
+//add cors middleware
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+  }),
+);
+//body parser middleware
+app.use(exp.json());
+//emp api middleware
+app.use("/emp-api", empRoute);
+
+//DB connection
+const connectDB = async () => {
+  try {
+    console.log(
+      process.env.MONGO_URL
+        ? "MONGO_URL found in environment"
+        : "MONGO_URL not found, using local MongoDB URL",
+    );
+    await connect(MONGO_URL, { serverSelectionTimeoutMS: 10000 });
+    console.log("DB connected");
+    app.listen(PORT, () => console.log(`server listening on port ${PORT}..`));
+  } catch (err) {
+    console.error("err in DB connection:", err.message);
+    console.error(err.stack);
+    process.exit(1);
+  }
+};
+
+connectDB();
+
+//error handling middleware
+app.use((err, req, res, next) => {
+  console.log("err in middleware:", err.message);
+
+  res.status(err.status || 500).json({
+    message: "error",
+    reason: err.message,
+  });
+});
+
